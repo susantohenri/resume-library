ae5a8ff08a5be48ca518430f54bf74d4391815a7
Henri Susanto <henry.dinus@gmail.com>
2020-06-05 09:20:41 +0700

    better form

--- resume-library.php
+++ resume-library.php
@@ -19,67 +19,166 @@ function resume_library_shortcode () {
 
 add_shortcode( 'resume_library_candidate_registration_form', 'resume_library_shortcode' );
 
-function resume_library_text_field_list () {
+function resume_library_form_elements () {
 	return array (
-	  "first_name" => "First Name :* ",
-	  "last_name" => "Last Name :*",
-	  "home_town" => "Home Town :*",
-	  "email" => "Email :*",
-	  "phone" => "Phone :*",
-	  "zip_code" => "Zip code :*",
-	  "latest_job_title" => "Latest job title :*",
+		array (
+			'name' => 'first_name',
+			'type' => 'text',
+			'label'=> 'First Name',
+			'required' => true
+		),
+		array (
+			'name' => 'last_name',
+			'type' => 'text',
+			'label'=> 'Last Name',
+			'required' => true
+		),
+		array (
+			'name' => 'home_town',
+			'type' => 'text',
+			'label'=> 'Home Town',
+			'required' => true
+		),
+		array (
+			'name' => 'email',
+			'type' => 'text',
+			'label'=> 'Email',
+			'required' => true
+		),
+		array (
+			'name' => 'phone',
+			'type' => 'text',
+			'label'=> 'Phone',
+			'required' => true
+		),
+		array (
+			'name' => 'zip_code',
+			'type' => 'text',
+			'label'=> 'Zip code',
+			'required' => true
+		),
+		array (
+			'name' => 'latest_job_title',
+			'type' => 'text',
+			'label'=> 'Latest job title',
+			'required' => true
+		),
+
+		array (
+			'name' => 'acceptable_job_types',
+			'type' => 'select-multiple',
+			'label'=> 'Acceptable Job Types',
+			'required' => true,
+			'options' => array ('Permanent', 'Contract', 'Temp', 'Part Time')
+		),
+
+		array (
+			'name' => 'uploaded_resume',
+			'type' => 'file',
+			'label'=> 'Resume',
+			'required' => true
+		),
+		array (
+			'name' => 'authorized_work_united_states',
+			'type' => 'checkbox',
+			'label'=> 'Are you authorized to work in the United States ?',
+			'required' => true
+		),
+		array (
+			'name' => 'resume_library_submit',
+			'type' => 'submit',
+			'label'=> 'Submit',
+			'required' => true
+		),
 	);
-}
-
-function resume_library_text_field ($name, $label) {
-	$value = isset ( $_POST[$name] ) ? esc_attr ( $_POST[$name] ) : '';
-	$required = strpos($label, '*') > -1 ? 'required="required"' : '';
-
-	return "
-		<p>
-			<label>{$label}</label>
-			<input type=\"text\" name=\"{$name}\" value=\"{$value}\" size=\"40\" {$required} />
-		</p>
-	";
-}
-
-function resume_library_upload_field ($name, $label) {
-	$value = isset ( $_POST[$name] ) ? esc_attr ( $_POST[$name] ) : '';
-	$required = strpos ( $label, '*' ) > -1 ? 'required="required"' : '';
-
-	return "
-		<p>
-			<label>{$label}</label>
-			<input type=\"file\" name=\"{$name}\" value=\"{$value}\" size=\"40\" {$required} />
-			<small>
-				<br/>
-				Acceptable file types: doc, docx, pdf, txt, gif, jpg, jpeg, png.
-				<br/>
-				Maximum file size: 1mb.
-			</small>
-		</p>
-	";
-}
-
-function resume_library_checkbox_field ($name, $label) {
-	$value = isset ( $_POST[$name] ) ? esc_attr ( $_POST[$name] ) : '';
-	return "
-		<p>
-			<input type=\"checkbox\" name=\"{$name}\" value=\"{$value}\">
-			<label>{$label}</label>
-		</p>
-	";
 }
 
 function resume_library_form () {
 	echo '<p>Upload your resume today to be headhunted in 50 different industries and apply for 1000s of jobs with 1-click apply.</p>';
 	echo '<p><small>* indicates required field</small></p>';
 	echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="POST" enctype="multipart/form-data">';
-	foreach (resume_library_text_field_list () as $name => $label) echo resume_library_text_field ($name, $label);
-	echo resume_library_upload_field ('uploaded_resume', 'Resume :*');
-	echo resume_library_checkbox_field ('authorized_work_united_states', 'Are you authorized to work in the United States ? :*');
-	echo '<input type="submit" value="Submit" name="resume_library_submit" >';
+
+	foreach ( resume_library_form_elements () as $field ) {
+		$name = $field['name'];
+		$type = $field['type'];
+		$label = $field['required'] ? $field['label'] . ' :*' : $field['required'] . ' :' ;
+		$required = $field['required'] ? 'required = "required"' : '';
+		$value = isset ( $_POST[$name] ) ? esc_attr ( $_POST[$name] ) : '';
+
+		switch ( $type ) {
+			case 'file':
+				echo "
+					<p>
+						<label>{$label}</label>
+						<input type=\"file\" name=\"{$name}\" value=\"{$value}\" size=\"40\" {$required} />
+						<small>
+							<br/>
+							Acceptable file types: doc, docx, pdf, txt, gif, jpg, jpeg, png.
+							<br/>
+							Maximum file size: 1mb.
+						</small>
+					</p>
+				";
+				break;
+			case 'checkbox':
+				echo "
+					<p>
+						<input type=\"checkbox\" name=\"{$name}\" value=\"{$value}\">
+						<label>{$label}</label>
+					</p>
+				";
+				break;
+			case 'submit':
+				echo "<input type=\"submit\" value=\"{$label}\" name=\"{$name}\" >";
+				break;
+			case 'select-multiple':
+				$options = '';
+				foreach ($field['options'] as $option) $options .= "<option>{$option}</option>";
+				echo "
+					<p>
+						<label>{$label}</label>
+						<select name=\"{$name}\" value=\"{$value}\" {$required} multiple=\"multiple\">
+							{$options}
+						</select>
+					</p>
+				";
+				break;
+			default:
+				echo "
+					<p>
+						<label>{$label}</label>
+						<input type=\"text\" name=\"{$name}\" value=\"{$value}\" size=\"40\" {$required} />
+					</p>
+				";
+				break;
+		}
+	}
+
 	echo '</form>';
+}
+
+function resume_library_form_submission_handler () {
+
+	$values = array_map(function ($field) {
+		return sanitize_text_field ( $_POST[$field['name']] );
+	}, resume_library_form_elements ());
+
+	$base64 = resume_library_uploaded_file_toBas64 ('uploaded_resume');
+
+	if ( 0 === count ($base64['error']) ) {
+		if ( isset ( $_POST['authorized_work_united_states'] ) ) {
+			$values['uploaded_resume'] = $base64['data'];
+			$response = resume_library_curl ($values);
+			echo '<p>';
+			echo json_encode($response);
+			echo '</p>';
+		}
+		echo "<p>Thanks {$values['first_name']}! submission success.</p>";
+	} else {
+		echo '<p>';
+		foreach ( $base64['error'] as $error ) echo "{$error} <br/>";
+		echo '</p>';
+	}
 }
 
 function resume_library_uploaded_file_toBas64 ($name) {
@@ -104,30 +203,6 @@ function resume_library_uploaded_file_toBas64 ($name) {
   		'resume_filetype' => $file_ext
   	)
   );
-}
-
-function resume_library_form_submission_handler () {
-	$values = [];
-	foreach ( resume_library_text_field_list () as $field => $label ) {
-		$values[$field] = sanitize_text_field ( $_POST[$field] );
-	}
-
-	$base64 = resume_library_uploaded_file_toBas64 ('uploaded_resume');
-
-	if ( 0 === count ($base64['error']) ) {
-		if ( isset ( $_POST['authorized_work_united_states'] ) ) {
-			$values['uploaded_resume'] = $base64['data'];
-			$response = resume_library_curl ($values);
-			echo '<p>';
-			echo json_encode($response);
-			echo '</p>';
-		}
-		echo "<p>Thanks {$values['first_name']}! submission success.</p>";
-	} else {
-		echo '<p>';
-		foreach ( $base64['error'] as $error ) echo "{$error} <br/>";
-		echo '</p>';
-	}
 }
 
 function resume_library_curl ($data) {
